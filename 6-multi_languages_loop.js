7b91e697f50f86e9aba9b7ab5ae74074be859217
sertse-bit <serhamre@gmail.com>
2025-07-17 13:43:49 +0300

    Add 6-multi_languages_loop.js: print languages using array and loop

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const messages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i = 0;
+let output = '';
+
+while (i < messages.length) {
+  output += messages[i] + '\n';
+  i++;
+}
+
+console.log(output.trim());
